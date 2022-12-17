370afc82fc10e9a0d50c133e8916ab6b0af26637
jesterb0206 <jesterb@seattleu.edu>
2022-12-17 00:56:01 -0800

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -27,6 +27,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  /* if (id > tours.length) { */
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
