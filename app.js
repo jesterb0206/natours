79b79c241e7fbc564163fd0d887e9acf23c65545
jesterb0206 <jesterb@seattleu.edu>
2022-12-17 04:49:01 -0800

    Setting up environment variables

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middleware
+if (process.env.NODE_ENV !== 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
