3622cf4bd8942cfdc946d1ad114a4705457a6f64
jesterb0206 <jesterb@seattleu.edu>
2022-12-17 01:11:01 -0800

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -70,6 +70,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here ...>',
+    },
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
