20e258b43e57e0b29a9c276cf9d3d232dd14b991
jesterb0206 <jesterb@seattleu.edu>
2022-12-17 02:41:02 -0800

    Created and Mounted Multiple Routers

--- app.js
+++ app.js
@@ -146,21 +146,19 @@ const deleteUser = (req, res) => {
 
 // Routes
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app.route('/api/v1/users/').get(getAllUsers).post(createUser);
+tourRouter.route(':id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
+
+userRouter.route(':id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // Start server
 
