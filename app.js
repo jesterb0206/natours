acb736920844505ce789b5848d7ad3495275fd08
jesterb0206 <jesterb@seattleu.edu>
2022-12-17 01:16:08 -0800

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -86,6 +86,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
