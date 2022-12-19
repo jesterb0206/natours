e51e091fadd88e09e71a6fa57b321ab7dd4515a8
jesterb0206 <jesterb@seattleu.edu>
2022-12-18 22:09:08 -0800

    Connected database to Express app

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('DB connection established!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App listening on ${port}...`);
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${port}...`);
 });
