8e3b57e55345c69ec65487f398fea72d2e6cc743
jesterb0206 <jesterb@seattleu.edu>
2022-12-18 22:24:02 -0800

    Created a basic Tour model

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ mongoose
     console.log('DB connection established!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
