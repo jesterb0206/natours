d8d7d9217af61dae392179cefb4c598ff3b968e8
jesterb0206 <jesterb@seattleu.edu>
2022-12-18 23:58:02 -0800

    Another way of reading documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,33 +1,40 @@
-// eslint-disable-next-line no-unused-vars
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    // tours,
-    // },
-  });
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log(req.params);
-  // const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  // status: 'success',
-  // data: {
-  // tour,
-  // },
-  // });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
