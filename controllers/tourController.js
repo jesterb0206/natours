d1845299bb3d8698491d7acd19dac53f6da87a27
jesterb0206 <jesterb@seattleu.edu>
2022-12-20 19:21:02 -0800

    Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // eslint-disable-next-line no-console
