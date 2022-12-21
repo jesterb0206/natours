0fa11c0742bf705f9bc5532322d03745522ba1c9
jesterb0206 <jesterb@seattleu.edu>
2022-12-20 22:59:01 -0800

    Data Validation: Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have no more than 40 characters!'],
+      minlength: [10, 'A tour name must have at least 10 characters!'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Valid values for difficulty are either easy, medium or hard!',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'A rating must be a number greater than or equal to 1!'],
+      max: [5, 'A rating must be a number less than or equal to 5!'],
     },
     ratingsQuantity: {
       type: Number,
