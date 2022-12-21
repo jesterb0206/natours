fc2f8f42c5058aa3ccdbb4b638599ddac7fae233
jesterb0206 <jesterb@seattleu.edu>
2022-12-21 01:07:01 -0800

    Better Errors and Refactoring

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// eslint-disable-next-line no-unused-vars
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
+// eslint-disable-next-line no-unused-vars
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,26 +28,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
