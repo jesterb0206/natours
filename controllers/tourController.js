09c690cfbf832aafdbd4f28531bcd6cb5cd7dee5
jesterb0206 <jesterb@seattleu.edu>
2022-12-20 18:52:02 -0800

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,6 +40,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) PAGINATION
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!');
+    }
+
     // EXECUTE QUERY
 
     const tours = await query;
