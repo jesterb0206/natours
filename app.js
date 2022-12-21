bf1efb8dd1015075d8c08789f8777cad9af1dd6a
jesterb0206 <jesterb@seattleu.edu>
2022-12-21 00:17:01 -0800

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
@@ -16,11 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from middleware!');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,5 +25,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
