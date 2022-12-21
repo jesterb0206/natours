7cc399b907e52d08413c7aec5ad7c75b2b0623d9
jesterb0206 <jesterb@seattleu.edu>
2022-12-20 16:07:21 -0800

    Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,10 +1,20 @@
-// eslint-disable-next-line no-unused-vars
-const { error } = require('console');
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+
+    const tours = await query;
+
+    // SEND RESPONSE
 
     res.status(200).json({
       status: 'success',
