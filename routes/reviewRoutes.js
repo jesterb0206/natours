9e1b62ffd90428f2d31f4e337474da9ea33f6489
jesterb0206 <jesterb@seattleu.edu>
2022-12-23 20:46:01 -0800

    Nested Routes with Express

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -24,14 +26,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
