34d68c7ebdca7db49460ac55e2a0214563eb67e0
jesterb0206 <jesterb@seattleu.edu>
2022-12-23 18:52:01 -0800

    Populating Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,18 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
