f0418e6f219e05ea0f0705c7be873873552c8ebd
jesterb0206 <jesterb@seattleu.edu>
2022-12-24 01:00:01 -0800

    Adding a /me Endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create an error if the user POSTs password data
 
