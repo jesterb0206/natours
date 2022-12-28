4e1afdafddb73a79efdced237c9141e5c4845166
jesterb0206 <jesterb@seattleu.edu>
2022-12-28 14:45:24 -0800

    Building the Tour Overview - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.set('Content-Security-Policy', "default-src 'self'");
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.set('Content-Security-Policy', "default-src 'self'");
