81da5acea1c7d577c4bf19a6dcefcbe91087453f
jesterb0206 <jesterb@seattleu.edu>
2022-12-29 02:40:11 -0800

    Building the Tour Page - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,8 +10,13 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
