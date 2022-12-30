4cdaa60698dbd032375153413d3791900b838250
jesterb0206 <jesterb@seattleu.edu>
2022-12-30 03:23:38 -0800

    Building the User Account Page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -32,3 +32,9 @@ exports.getLoginForm = (req, res) => {
     title: 'Log Into Your Account',
   });
 };
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account',
+  });
+};
