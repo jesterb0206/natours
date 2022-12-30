da547d1d09ea4f9656a4f5a23358d9c6a80e863d
jesterb0206 <jesterb@seattleu.edu>
2022-12-29 20:30:08 -0800

    Building the Login Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,7 +10,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
@@ -20,3 +20,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log Into Your Account',
+  });
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getLoginForm);
 
 module.exports = router;
