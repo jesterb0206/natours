357311a05f02bf1fed444433d9e4487a739c1b00
jesterb0206 <jesterb@seattleu.edu>
2022-12-31 16:22:02 -0800

    Email Templates with Pug: Welcome Emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -44,9 +44,12 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
-    role: req.body.role,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  // eslint-disable-next-line no-console
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -54,15 +57,14 @@ exports.signup = catchAsync(async (req, res, next) => {
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  // 1) CHECK IF EMAIL AND PASSWORD EXIST
+  // 1) Check if the email and password exists
 
   if (!email || !password) {
     return next(
       new AppError('Please provide a valid email address and password!', 400)
     );
   }
-
-  // 2) CHECK IF THE USER EXISTS AND THE PASSWORD IS CORRECT
+  // 2) Check if the user exists and the password is correct
 
   const user = await User.findOne({ email }).select('+password');
 
@@ -70,7 +72,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password!', 401));
   }
 
-  // 3) IF EVERYTHING IS OKAY, SEND THE TOKEN TO THE CLIENT
+  // 3) If everything's ok, send the token to client
 
   createSendToken(user, 200, res);
 });
@@ -206,11 +208,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 minutes)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 minutes)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
