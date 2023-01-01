c0f818cd1197a7ab48217266b133613ed956d1db
jesterb0206 <jesterb@seattleu.edu>
2022-12-31 21:29:28 -0800

    Rendering a User's Booked Tours

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -15,6 +15,7 @@ router.get(
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
