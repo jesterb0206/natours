c42cd6e01e1e0a753eea6a3f8b13c3cbbe64e69e
jesterb0206 <jesterb@seattleu.edu>
2023-03-07 15:13:01 -0800

    Deleted Mapbox & Unnecessary Comments

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,10 @@
 import '@babel/polyfill';
-import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 
-const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
@@ -14,11 +12,6 @@ const userPasswordForm = document.querySelector('.form-user-password');
 const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
-
-if (mapBox) {
-  const locations = JSON.parse(mapBox.dataset.locations);
-  displayMap(locations);
-}
 
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
