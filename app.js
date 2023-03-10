0141d5daece678ecccaa68f5b4894355309a3909
jesterb0206 <jesterb@seattleu.edu>
2023-03-10 15:46:01 -0800

    Changed package.json Description, Changed server.js Console Log Messages, and Tidied up app.js

--- app.js
+++ app.js
@@ -1,104 +1,42 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
-// const mongoSanitize = require('express-mongo-sanitize');
-// const xss = require('xss-clean');
-// const hpp = require('hpp');
-const cookieParser = require('cookie-parser');
-const compression = require('compression');
-const cors = require('cors');
-
 const AppError = require('./utils/appError');
+const app = express();
+const bookingRouter = require('./routes/bookingRoutes');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
 const globalErrorHandler = require('./controllers/errorController');
+const morgan = require('morgan');
+const path = require('path');
+const reviewRouter = require('./routes/reviewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
-const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
-
-const app = express();
 
 app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware
-
-// Implement CORS
-
-// Access-Control-Allow-Origin *
-
 app.use(cors());
 
 app.options('*', cors());
 
-// Serves static files
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Set security HTTP headers
-
-// app.use(helmet());
-
-// Development Logging
 
 if (process.env.NODE_ENV !== 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit number of requests from the same IP address
-
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message:
-//     'Too many requests from this IP address, please try again in an hour!',
-// });
-
-// app.use('/api', limiter);
-
-// Body parser, which reads data from the body and puts it into req.body
-
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
-
-// Data sanitization against NoSQL query injection
-
-// app.use(mongoSanitize());
-
-// Data sanitization against XSS
-
-// app.use(xss());
-
-// Prevent parameter pollution
-
-// app.use(
-//   hpp({
-//     whitelist: [
-//       'duration',
-//       'ratingsQuantity',
-//       'ratingsAverage',
-//       'maxGroupSize',
-//       'difficulty',
-//       'price',
-//     ],
-//   })
-// );
-
 app.use(compression());
-
-// Testing middleware
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// Routes
 
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log('Uncaught exception, shutting down!');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -23,16 +24,16 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => {
-    console.log('DB connection established!');
+    console.log('Database connection established!');
   });
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App listening on port ${port}...`);
+  console.log(`App listening on port ${port}!`);
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log('Unhandled rejection, shutting down!');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
@@ -40,8 +41,8 @@ process.on('unhandledRejection', (err) => {
 });
 
 process.on('SIGTERM', () => {
-  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully!');
+  console.log('SIGTERM received, shutting down gracefully!');
   server.close(() => {
-    console.log('💥 Process terminated!');
+    console.log('Process terminated!');
   });
 });
