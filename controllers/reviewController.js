9408d0764f744795fd22b5f700779eb859c29bf2
jesterb0206 <jesterb@seattleu.edu>
2023-03-09 20:04:03 -0800

    Updated reviewController to ES6 Module

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,16 +1,16 @@
-const Review = require('../models/reviewModel');
-const factory = require('./handlerFactory');
+import Review from './../models/reviewModel.js';
+import * as factory from './handlerFactory.js';
 
-exports.setTourUserIds = (req, res, next) => {
-  // Allow nested routes
-
+export const setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
+
   if (!req.body.user) req.body.user = req.user.id;
+
   next();
 };
 
-exports.getAllReviews = factory.getAll(Review);
-exports.getReview = factory.getOne(Review);
-exports.createReview = factory.createOne(Review);
-exports.updateReview = factory.updateOne(Review);
-exports.deleteReview = factory.deleteOne(Review);
+export const createReview = factory.createOne(Review);
+export const deleteReview = factory.deleteOne(Review);
+export const getReview = factory.getOne(Review);
+export const getAllReviews = factory.getAll(Review);
+export const updateReview = factory.updateOne(Review);
