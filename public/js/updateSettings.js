15b84ac07866888e68039cfd41e53b4cd8506703
jesterb0206 <jesterb@seattleu.edu>
2023-03-10 20:05:02 -0800

    Window Reload Account Page

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -16,6 +16,9 @@ export const updateSettings = async (data, type) => {
 
     if (res.data.status === 'success') {
       showAlert('success', `${type.toUpperCase()} updated successfully!`);
+      window.setTimeout(() => {
+        location.assign('/me');
+      }, 1500);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
