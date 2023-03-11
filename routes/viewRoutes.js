7fb95bcd92839d770313e2c76c081d6ef848efb8
jesterb0206 <jesterb@seattleu.edu>
2023-03-10 20:41:02 -0800

    Booking Controller Was Undefined In View Routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,22 +1,22 @@
-const authController = require('../controllers/authController');
 const express = require('express');
-const router = express.Router();
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
-router.use(viewsController.alerts);
+const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignUpForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get(
-  '/my-tours',
-  bookingController.createBookingCheckout,
-  authController.protect,
-  viewsController.getMyTours
-);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
